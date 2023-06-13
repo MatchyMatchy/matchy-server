7f201b19f45f5fb6ad92d365d6fa8c90fe814533
Nonso <nonsootoh@gmail.com>
2023-06-12 21:47:49 -0400

    chore: adds socket io

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import Express from "express";
 import Cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = Express();
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(
   Cors({
     origin: "*",
@@ -9,6 +13,10 @@ app.use(
   })
 );
 
-app.listen(process.env.PORT, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`server is active on port ${process.env.PORT}`);
 });
